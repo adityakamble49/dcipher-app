3ddaef25e1f41fa2fc932c9fd70fbe346eb10ddf
Aditya Kamble <adityakamble49@gmail.com>
2017-12-08 21:51:51 +0530

    Encrypt and Decrypt Text activity added

---
+++ app/src/main/java/com/adityakamble49/mcrypt/ui/DecryptActivity.kt
@@ -0,0 +1,13 @@
+package com.adityakamble49.mcrypt.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.adityakamble49.mcrypt.R
+
+class DecryptActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_decrypt)
+    }
+}
---
+++ app/src/main/java/com/adityakamble49/mcrypt/ui/EncryptActivity.kt
@@ -0,0 +1,13 @@
+package com.adityakamble49.mcrypt.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.adityakamble49.mcrypt.R
+
+class EncryptActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_encrypt)
+    }
+}
