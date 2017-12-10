3ded94a16510d0b0e45ee8446c529f3bdc74423a
Aditya Kamble <adityakamble49@gmail.com>
2017-12-10 23:34:13 +0530

    Key Manager activity back action added

--- app/src/main/java/com/adityakamble49/mcrypt/ui/KeyManagerActivity.kt
+++ app/src/main/java/com/adityakamble49/mcrypt/ui/KeyManagerActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
+import android.view.MenuItem
 import com.adityakamble49.mcrypt.R
 import kotlinx.android.synthetic.main.activity_key_manager.*
 
@@ -15,6 +16,10 @@ import kotlinx.android.synthetic.main.activity_key_manager.*
  */
 class KeyManagerActivity : AppCompatActivity() {
 
+    /*
+     * Lifecycle Functions
+     */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_key_manager)
@@ -22,7 +27,27 @@ class KeyManagerActivity : AppCompatActivity() {
         bindView()
     }
 
+
+    /*
+     * Listener Functions
+     */
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> finish()
+        }
+        return true
+    }
+
+
+    /*
+     * Helper Functions
+     */
+
     private fun bindView() {
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         val linearLayoutManager = LinearLayoutManager(this)
         val decorator = DividerItemDecoration(this, linearLayoutManager.orientation)
         val rsaKeyListAdapter = RSAKeyListAdapter()
