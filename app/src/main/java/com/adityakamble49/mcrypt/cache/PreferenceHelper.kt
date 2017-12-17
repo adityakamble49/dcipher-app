a1fc0c6c5b67326b32c24470b06c9777a5868c92
Aditya Kamble <adityakamble49@gmail.com>
2017-12-17 16:48:00 +0530

    Preference Helper added

---
+++ app/src/main/java/com/adityakamble49/mcrypt/cache/PreferenceHelper.kt
@@ -0,0 +1,31 @@
+package com.adityakamble49.mcrypt.cache
+
+import android.content.Context
+import android.content.SharedPreferences
+import javax.inject.Inject
+
+/**
+ * Preference Helper
+ *
+ * @author Aditya Kamble
+ * @since 17/12/2017
+ */
+class PreferenceHelper @Inject constructor(context: Context) {
+
+    companion object {
+        private val PREF_QUOTES_PACKAGE_NAME = "com.adityakamble49.mcrypt"
+
+        private val PREF_KEY_CURRENT_RSA_KEY_ID = "current_rsa_key_id"
+    }
+
+    private val mcryptPref: SharedPreferences
+
+    init {
+        mcryptPref = context.getSharedPreferences(PREF_QUOTES_PACKAGE_NAME, Context.MODE_PRIVATE)
+    }
+
+    var currentRSAKeyId: Int
+        get() = mcryptPref.getInt(PREF_KEY_CURRENT_RSA_KEY_ID, 0)
+        set(currentKeyId) = mcryptPref.edit().putInt(PREF_KEY_CURRENT_RSA_KEY_ID,
+                currentKeyId).apply()
+}
