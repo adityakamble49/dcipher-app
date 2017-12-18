be7715fff819d14806a8a785174947dc267205e3
Aditya Kamble <adityakamble49@gmail.com>
2017-12-18 22:38:23 +0530

    Change Key action added to Loaded Key View

--- app/src/main/java/com/adityakamble49/mcrypt/ui/EncryptActivity.kt
+++ app/src/main/java/com/adityakamble49/mcrypt/ui/EncryptActivity.kt
@@ -1,19 +1,22 @@
 package com.adityakamble49.mcrypt.ui
 
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.adityakamble49.mcrypt.R
 import com.adityakamble49.mcrypt.model.RSAKeyPair
 import com.adityakamble49.mcrypt.ui.common.CommonViewModel
 import com.adityakamble49.mcrypt.ui.common.CommonViewModelFactory
+import com.adityakamble49.mcrypt.ui.keys.KeyManagerActivity
 import dagger.android.AndroidInjection
 import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_encrypt.*
 import javax.inject.Inject
 
-class EncryptActivity : AppCompatActivity() {
+class EncryptActivity : AppCompatActivity(), View.OnClickListener {
 
     // Dagger Injected Fields
     @Inject lateinit var commonViewModelFactory: CommonViewModelFactory
@@ -40,11 +43,21 @@ class EncryptActivity : AppCompatActivity() {
     }
 
     /*
+     * Listener Functions
+     */
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.change_rsa_key -> startActivity(Intent(this, KeyManagerActivity::class.java))
+        }
+    }
+
+    /*
      * Helper Functions
      */
 
     private fun bindView() {
-
+        change_rsa_key.setOnClickListener(this)
     }
 
     private inner class GetCurrentRSAKeyPairSubscriber : Observer<RSAKeyPair> {
