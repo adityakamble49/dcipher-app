7a780fbf8a558646888f1a1adb0d67e4aa1a4cce
Aditya Kamble <adityakamble49@gmail.com>
2017-12-31 23:46:00 +0530

    Decrypt text handled in DecryptActivity - opening file and direct text support

--- app/src/main/java/com/adityakamble49/mcrypt/ui/decrypt/DecryptActivity.kt
+++ app/src/main/java/com/adityakamble49/mcrypt/ui/decrypt/DecryptActivity.kt
@@ -4,8 +4,10 @@ import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Toast
 import com.adityakamble49.mcrypt.R
 import com.adityakamble49.mcrypt.cache.exception.EncryptionKeyNotFoundException
 import com.adityakamble49.mcrypt.model.EncryptionKey
@@ -32,6 +34,7 @@ class DecryptActivity : AppCompatActivity(), View.OnClickListener {
 
     // Other Fields
     var currentEncryptionKey: EncryptionKey? = null
+    var isDecrypted = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,6 +66,7 @@ class DecryptActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(view: View?) {
         when (view?.id) {
+            R.id.decrypt_button -> handleDecryptText()
             R.id.change_encryption_key -> startActivity(
                     Intent(this, KeyManagerActivity::class.java))
         }
@@ -74,6 +78,7 @@ class DecryptActivity : AppCompatActivity(), View.OnClickListener {
      */
 
     private fun bindView() {
+        decrypt_button.setOnClickListener(this)
         change_encryption_key.setOnClickListener(this)
     }
 
@@ -120,5 +125,46 @@ class DecryptActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun handleEncryptedTextFromFile(encryptedText: String) {
         input_text.setText(encryptedText)
+    }
+
+    private fun handleDecryptText() {
+        if (isDecrypted) {
+            Toast.makeText(this, "Text Already Decrypted", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (currentEncryptionKey == null) {
+            Toast.makeText(this, "Key not loaded", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val textToEncrypt = input_text.text.toString()
+        if (textToEncrypt.isEmpty()) {
+            Toast.makeText(this, "Text Empty!", Toast.LENGTH_SHORT).show()
+            return
+        }
+        currentEncryptionKey?.let {
+            decryptViewModel.decryptText(it, textToEncrypt, DecryptTextSubscriber())
+        }
+    }
+
+    private inner class DecryptTextSubscriber : SingleObserver<String> {
+        override fun onSubscribe(d: Disposable) {}
+
+        override fun onSuccess(decryptedText: String) {
+            handleDecryptedText(decryptedText)
+            Toast.makeText(this@DecryptActivity, "Decryption Successful", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onError(e: Throwable) {
+            Timber.i(e)
+            Toast.makeText(this@DecryptActivity, "Decryption Failed", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun handleDecryptedText(decryptedText: String) {
+        isDecrypted = true
+        input_text.setText(decryptedText)
+        input_text.isEnabled = false
+        input_text.setBackgroundResource(R.color.white)
+        input_text.setTextColor(ContextCompat.getColor(this, R.color.light_black))
     }
 }
