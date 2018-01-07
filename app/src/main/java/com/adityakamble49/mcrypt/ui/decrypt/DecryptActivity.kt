f0328ee27f3e43223c15b9832c7eae1ec7ec505a
Aditya Kamble <adityakamble49@gmail.com>
2018-01-07 10:43:13 +0530

    Save Decryption handled

--- app/src/main/java/com/adityakamble49/mcrypt/ui/decrypt/DecryptActivity.kt
+++ app/src/main/java/com/adityakamble49/mcrypt/ui/decrypt/DecryptActivity.kt
@@ -1,11 +1,16 @@
 package com.adityakamble49.mcrypt.ui.decrypt
 
+import android.Manifest
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.text.InputType
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -16,15 +21,22 @@ import com.adityakamble49.mcrypt.model.EncryptionKey
 import com.adityakamble49.mcrypt.ui.common.CommonViewModel
 import com.adityakamble49.mcrypt.ui.common.CommonViewModelFactory
 import com.adityakamble49.mcrypt.ui.keys.KeyManagerActivity
+import com.adityakamble49.mcrypt.utils.hasSpecialChar
+import com.afollestad.materialdialogs.DialogAction
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.folderselector.FolderChooserDialog
 import dagger.android.AndroidInjection
+import io.reactivex.CompletableObserver
 import io.reactivex.Observer
 import io.reactivex.SingleObserver
 import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_decrypt.*
 import timber.log.Timber
+import java.io.File
 import javax.inject.Inject
 
-class DecryptActivity : AppCompatActivity(), View.OnClickListener {
+class DecryptActivity : AppCompatActivity(), View.OnClickListener,
+        FolderChooserDialog.FolderCallback {
 
     // Dagger Injected Fields
     @Inject lateinit var commonViewModelFactory: CommonViewModelFactory
@@ -35,8 +47,13 @@ class DecryptActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var decryptViewModel: DecryptViewModel
 
     // Other Fields
+    val PERMISSION_WRITE = Manifest.permission.WRITE_EXTERNAL_STORAGE
+    val PERMISSION_LIST = arrayOf(PERMISSION_WRITE)
+    val REQ_SAVE_DECRYPTED_FILE = 101
     var currentEncryptionKey: EncryptionKey? = null
     var isDecrypted = false
+    var currentDecryptedText: String? = null
+    var currentDecryptedTextFileName: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -82,9 +99,32 @@ class DecryptActivity : AppCompatActivity(), View.OnClickListener {
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when (item?.itemId) {
             R.id.action_reset -> handleResetDecryption()
+            R.id.action_save -> handleSaveDecryption()
             else -> return super.onOptionsItemSelected(item)
         }
         return true
+    }
+
+    override fun onFolderSelection(dialog: FolderChooserDialog, folder: File) {
+        currentDecryptedText?.let { text ->
+            currentDecryptedTextFileName?.let { fileName ->
+                decryptViewModel.saveDecryptedText(folder.absolutePath, fileName, text,
+                        SaveDecryptedTextToFileSubscriber())
+            }
+        }
+
+    }
+
+    override fun onFolderChooserDismissed(dialog: FolderChooserDialog) {
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,
+                                            grantResults: IntArray) {
+        when (requestCode) {
+            REQ_SAVE_DECRYPTED_FILE -> handleSaveDecryption()
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
     }
 
 
@@ -189,5 +229,74 @@ class DecryptActivity : AppCompatActivity(), View.OnClickListener {
         input_text.isEnabled = true
         input_text.setBackgroundResource(R.color.almost_black)
         input_text.setTextColor(ContextCompat.getColor(this, R.color.white))
+    }
+
+    private fun handleSaveDecryption() {
+        if (!isDecrypted) {
+            Toast.makeText(this, "Text not decrypted", Toast.LENGTH_LONG).show()
+            return
+        }
+        performExternalStorageOperation(REQ_SAVE_DECRYPTED_FILE, { saveDecryptedTextToFile() })
+    }
+
+    private fun saveDecryptedTextToFile() {
+        currentDecryptedText = input_text.text.toString()
+        buildSaveDecryptedFileNameDialog().show()
+    }
+
+    private fun buildSaveDecryptedFileNameDialog() = MaterialDialog
+            .Builder(this)
+            .title(getString(R.string.decrypted_file_dialog_title))
+            .content(getString(R.string.decrypted_file_dialog_content))
+            .inputType(InputType.TYPE_CLASS_TEXT)
+            .inputRange(1, 50)
+            .input(getString(R.string.decrypted_file_dialog_hint),
+                    getString(R.string.decrypted_file_dialog_prefill), { dialog, input ->
+                val keyName = input.toString()
+                if (keyName.hasSpecialChar()) {
+                    dialog.getActionButton(DialogAction.POSITIVE).isEnabled = false
+                    dialog.setContent(
+                            getString(R.string.decrypted_file_dialog_special_char_warning))
+                } else {
+                    dialog.getActionButton(DialogAction.POSITIVE).isEnabled = true
+                    dialog.setContent(getString(R.string.decrypted_file_dialog_content))
+                }
+            })
+            .onPositive { dialog, _ ->
+                dialog.inputEditText?.text.let {
+                    currentDecryptedTextFileName = it.toString()
+                    buildSaveDecryptedFileDirDialog().show(this)
+                }
+            }
+            .alwaysCallInputCallback()
+            .build()
+
+    private fun buildSaveDecryptedFileDirDialog() = FolderChooserDialog
+            .Builder(this)
+            .initialPath(Environment.getExternalStorageDirectory().absolutePath)
+            .build()
+
+    private fun performExternalStorageOperation(permissionReqCode: Int, operation: () -> Unit) {
+        if (ContextCompat.checkSelfPermission(this,
+                PERMISSION_WRITE) != PackageManager.PERMISSION_GRANTED) {
+            // Request permission
+            ActivityCompat.requestPermissions(this, PERMISSION_LIST, permissionReqCode)
+        } else {
+            // Perform operation
+            operation()
+        }
+    }
+
+    private inner class SaveDecryptedTextToFileSubscriber : CompletableObserver {
+        override fun onSubscribe(d: Disposable) {}
+
+        override fun onComplete() {
+            Toast.makeText(this@DecryptActivity, "Text Saved", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onError(e: Throwable) {
+            Timber.e(e)
+            Toast.makeText(this@DecryptActivity, "Text save failed", Toast.LENGTH_SHORT).show()
+        }
     }
 }
