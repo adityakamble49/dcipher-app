2be0e00cdba364c40dc38133bda7b4c8b61b312f
Aditya Kamble <adityakamble49@gmail.com>
2018-01-07 11:37:18 +0530

    Show Toast extension method added

--- app/src/main/java/com/adityakamble49/mcrypt/utils/ViewExtensions.kt
+++ app/src/main/java/com/adityakamble49/mcrypt/utils/ViewExtensions.kt
@@ -1,9 +1,12 @@
 package com.adityakamble49.mcrypt.utils
 
+import android.content.Context
 import android.support.annotation.LayoutRes
+import android.support.annotation.StringRes
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 /**
  * @author Aditya Kamble
@@ -16,4 +19,18 @@ import android.view.ViewGroup
  */
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View {
     return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
+}
+
+/**
+ * Show plain text with Toast
+ */
+fun Context.showToast(text: CharSequence) {
+    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+}
+
+/**
+ * Show resource text with Toast
+ */
+fun Context.showToast(@StringRes resId: Int) {
+    Toast.makeText(this, resId, Toast.LENGTH_SHORT).show()
 }
